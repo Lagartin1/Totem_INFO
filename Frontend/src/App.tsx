8c72657f0b3d8bb2f8cba62063fa22e023ca3708
Lagartin1 <francisco@LAPTOP-EB6IR2K2.localdomain>
2025-08-25 11:00:27 -0400

    tailwind funcionando

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -25,7 +25,10 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <div className="bg-blue-500 h-64">
+
+      </div>
+      <p className="mx-50">
         Click on the Vite and React logos to learn more
       </p>
     </>
