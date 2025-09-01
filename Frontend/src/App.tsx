0322400df0ed5b45cf1bc3f7cadfd46724ef8b95
Lagartin1 <andreslabrin10@gmail.com>
2025-09-01 09:42:59 -0400

    Funcionando Tailwind completo

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,37 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full bg-gray-100 h-full'>
+      <div className='flex justify-center items-center h-full'>
+        <h1 className='text-4xl font-bold'>Hola mundo</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="bg-blue-500 h-64">
-
-      </div>
-      <p className="mx-50">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
