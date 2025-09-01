2ba2b5240f528865696d44e89206579188567800
Lagartin1 <andreslabrin10@gmail.com>
2025-09-01 11:37:59 -0400

    Botones para landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import Boton_Landing from "./Components/Boton_Landig"
+
 
 function App() {
 
   return (
-    <div className='w-full bg-gray-100 h-full'>
-      <div className='flex justify-center items-center h-full'>
-        <h1 className='text-4xl font-bold'>Hola mundo</h1>
-      </div>
-    </div>
+    <main className='min-h-screen w-full flex flex-col items-center bg-white-500'>
+      <div className="container flex flex-col">
+          <div className="items-center grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 p-20">
+            <Boton_Landing Title="Practicas" Link="/practicas"/>
+            <Boton_Landing Title="Tesis" Link="/tesis"/>
+            <Boton_Landing Title="Proyectos" Link="/proyectos"/>
+            <Boton_Landing Title="Becados" Link="/becados"/>
+          </div>
+        </div>
+    </main>
   )
 }
 
