bda5a4bd7a406f0ee85d4698da8b5fe3f1ee86a5
Lagartin1 <andreslabrin10@gmail.com>
2025-09-02 15:30:43 -0400

    pagina practicas

--- Frontend/src/Components/Header.tsx
+++ Frontend/src/Components/Header.tsx
@@ -5,7 +5,7 @@ import logo_inf from "../assets/UACh_Informatica.png"
 function Header(){
     return(
         <header className="w-full flex flex-row items-center">
-            <img src={logo_uach} alt="Logo UACh" className="h-30 mx-4"/>
+            <img src={logo_uach} alt="Logo UACh" className="h-30 mx-20"/>
             <img src={logo_inf} alt="Logo UACh Informatica" className="h-24 mx-4"/>
         </header>
     )
