6e20dc791b3ad1697391308f4fb7fe686b9646b5
Huzs02 <johanfranciscomunoz@gmail.com>
2025-11-08 19:45:38 -0300

    Se agrego la ruta de noticias al main.tsx

--- FrontendAdmin/src/main.tsx
+++ FrontendAdmin/src/main.tsx
@@ -1,32 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './index.css'    
-import Home from './pages/Login';
-import Dashboard from './pages/dashboard';
-import { AuthProvider } from './lib/authProvider';
-import ProtectedRoute from './routes/protectedRoutes';
-import LoadData from './pages/loadData';
-import AdminPracticas from './pages/adminPracticas';
-import PracticasExistentes from './pages/practicasExistentes';
-import NotFound from './pages/404';
+import "./index.css";
+import Home from "./pages/Login";
+import Dashboard from "./pages/dashboard";
+import { AuthProvider } from "./lib/authProvider";
+import ProtectedRoute from "./routes/protectedRoutes";
+import LoadData from "./pages/loadData";
+import AdminPracticas from "./pages/adminPracticas";
+import PracticasExistentes from "./pages/practicasExistentes";
+import NotFound from "./pages/404";
+import NoticiasSection from "./pages/Noticias";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider> 
+    <AuthProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-            <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/load-data" element={<LoadData />} />
             <Route path="/admin-practicas" element={<AdminPracticas />} />
-            <Route path="/admin-practicas/:type" element={<PracticasExistentes />} />
-            </Route>
+            <Route
+              path="/admin-practicas/:type"
+              element={<PracticasExistentes />}
+            />
+            <Route path="/noticias" element={<NoticiasSection />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </AuthProvider>
   </StrictMode>
-)
+);
