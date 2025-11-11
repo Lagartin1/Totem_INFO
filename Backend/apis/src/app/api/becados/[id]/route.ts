8caa2a5a460e03875b8dfbd4bf3700119151c744
Lagartin1 <andreslabrin10@gmail.com>
2025-11-11 14:47:40 -0300

    cambios para que funcione en el server

--- Backend/apis/src/app/api/becados/[id]/route.ts
+++ Backend/apis/src/app/api/becados/[id]/route.ts
@@ -4,7 +4,7 @@ import { addLogEntry } from "@/models/admin/logModel";
 import { cookies } from "next/headers";
 import { verifyAccessToken, getUserIdFromSessionToken } from "@/lib/auth/login_tools";
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
     const jar = await cookies();  
     const token = jar.get("access_token")?.value;
@@ -20,9 +20,9 @@ export async function DELETE(request: Request, { params }: { params: { id: strin
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     
-    const { id } = params;
+    const { id } = await params;
     const response = await DeleteBecadosController(id);
-    await addLogEntry(userId, "deleteBecado", `Becado eliminado con ID: ${params.id}`);
+    await addLogEntry(userId, "deleteBecado", `Becado eliminado con ID: ${id}`);
     return NextResponse.json(
       { message: "Becado eliminado correctamente", response },
       { status: 200 }
@@ -36,7 +36,7 @@ export async function DELETE(request: Request, { params }: { params: { id: strin
   }
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
     const jar = await cookies();  
     const token = jar.get("access_token")?.value;
@@ -52,9 +52,9 @@ export async function PUT(request: Request, { params }: { params: { id: string }
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     
-    const { id } = params;
+    const { id } = await params;
     const body = await request.formData();
-    await addLogEntry(userId, "updateBecado", `Becado actualizado con ID: ${params.id}`);
+    await addLogEntry(userId, "updateBecado", `Becado actualizado con ID: ${id}`);
     const response = await PutBecadosController(id, body);
     return NextResponse.json(
       { message: "Becado actualizado correctamente", response },
