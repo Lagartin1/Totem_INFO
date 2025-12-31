d57caef1cd50e9aa7ba836e7769d8c926dfdb68d
MobNaico <nicolas.yevenes@alumnos.uach.cl>
2025-12-31 16:29:16 -0300

    Backup mongo

--- Backend/synchronizer/sincronize_practicas.py
+++ Backend/synchronizer/sincronize_practicas.py
@@ -16,7 +16,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# ==========================================================
+# ==========================================================    
 # Google Sheets (OAuth) - SOLO LECTURA (tu mismo correo)
 # ==========================================================
 # Scope readonly porque solo leemos
